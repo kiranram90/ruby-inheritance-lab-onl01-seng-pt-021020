50f1ba84fd0c4a15a9e34bea56bbaf76446dd37a
kiran ram <kram@lions.molloy.edu>
2020-03-24 02:21:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,13 +2,19 @@
 
 class Student < User
   
-  
+  attr_accessor :knowledge
   
 def initialize
   @knowledge = []
 end
 
 def learn(info)
+  knowledge << info
+end
+
+def knowledge 
+  knowledge 
+end
   
 
 end
